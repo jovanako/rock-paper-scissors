ab370b381271c2e26222f3126c70b1ee3d4c38cc
Jovana Kovacevic <jovana1005@gmail.com>
2021-10-21 20:56:33 +0200

    modified js

--- app.js
+++ app.js
@@ -1,50 +1,51 @@
-const userChoiceDisplay = document.createElement('h1')
-const computerChoiceDisplay = document.createElement('h1')
-const resultDisplay = document.createElement('h1')
-const gameGrid = document.getElementById('game')
+const userChoiceDisplay = document.createElement("h1")
+userChoiceDisplay.innerHTML = "User choice: "
+const computerChoiceDisplay = document.createElement("h1")
+computerChoiceDisplay.innerHTML = "Computer choice: "
+const resultDisplay = document.createElement("h1")
+resultDisplay.innerHTML = "&nbsp;"
+const gameGrid = document.getElementById("game")
 
 gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay)
 
-const choices = ['rock', 'paper', 'scissors']
-let userChoice
-let computerChoice
+const choices = ["rock", "paper", "scissors"]
 
 const handleClick = (e) => {
-  userChoice = e.target.id
-  userChoiceDisplay.innerHTML = 'User choice: ' + userChoice
-  generateComputerChoice()
-  getResults()
+  const userChoice = e.target.id
+  userChoiceDisplay.innerHTML = "User choice: " + userChoice
+  const computerChoice = generateComputerChoice()
+  getResults(userChoice, computerChoice)
 }
 
 const generateComputerChoice = () => {
-  const randomChoice = choices[Math.floor(Math.random() * choices.length)]
-  computerChoice = randomChoice
-  computerChoiceDisplay.innerHTML = 'Computer choice: ' + computerChoice
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)]
+  computerChoiceDisplay.innerHTML = "Computer choice: " + computerChoice
+  return computerChoice
 }
 
 for (let i = 0; i < choices.length; i++) {
-  const button = document.createElement('button')
+  const button = document.createElement("button")
   button.id = choices[i]
   button.innerHTML = choices[i]
-  button.addEventListener('click', handleClick)
+  button.addEventListener("click", handleClick)
   gameGrid.appendChild(button)
 }
 
-const getResults = () => {
+const getResults = (userChoice, computerChoice) => {
   switch (userChoice + computerChoice) {
-    case 'scissorspaper':
-    case 'rockscissors':
-    case 'paperrock':
+    case "scissorspaper":
+    case "rockscissors":
+    case "paperrock":
       resultDisplay.innerHTML = "YOU WIN!"
       break
-    case 'paperscissors':
-    case 'scissorsrock':
-    case 'rockpaper':
+    case "paperscissors":
+    case "scissorsrock":
+    case "rockpaper":
       resultDisplay.innerHTML = "YOU LOSE!"
       break
-    case 'paperpaper':
-    case 'scissorsscissors':
-    case 'rockrock':
+    case "paperpaper":
+    case "scissorsscissors":
+    case "rockrock":
       resultDisplay.innerHTML = "IT'S A DRAW!"
       break
   }
